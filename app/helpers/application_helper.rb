2336d098bd74efb1df7399c65e56668cd4d6d75c
Jeffrey Barszcz <jfbarszcz@gmail.com>
2015-01-07 10:57:50 -0500

    Login working now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def auth_token_input
+    "<input type=\"hidden\" name=\"authenticity_token\" value=\"#{ form_authenticity_token}\">"
+    .html_safe
+  end
+
+  def method_input(method)
+    "<input type=\"hidden\" name=\"_method\" value=\"#{ method.to_s }\">"
+    .html_safe
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,11 @@ class User < ActiveRecord::Base
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.correct_password?(password) ? user : nil
+  end
+
   def reset_session_token!
     self.session_token = SecureRandom.urlsafe_base64
     self.save!

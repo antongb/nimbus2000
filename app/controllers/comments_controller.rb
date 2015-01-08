883aceafc0753dbd04cbde1beaf491cd8c981130
Jeffrey Barszcz <jfbarszcz@gmail.com>
2015-01-08 10:45:29 -0500

    can now edit comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :ensure_author, only: [:edit, :update, :destroy]
+
   def index
     redirect_to track_url(params[:track_id])
   end
@@ -15,14 +17,30 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
     @comment.destroy
     redirect_to track_url(@comment.track_id)
+  end
+
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to track_url(@comment.track_id)
+    else
+      flash[:errors] = @comment.errors.full_messages
+      redirect_to track_url(@comment.track_id)
+    end       
   end
 
   private
 
   def comment_params
     params.require(:comment).permit([:body, :parent_id, :track_id])
+  end
+
+  def ensure_author
+    @comment = Comment.find(params[:id])
+    redirect_to root_url unless current_user.id == @comment.user_id
   end
 end

85827999ab00f3e48d3f52dea2157d11ee0b7ffc
Jeffrey Barszcz <jfbarszcz@gmail.com>
2015-01-09 17:44:05 -0500

    Adds header to Backbone (real nav view coming soon), can now delete tracks

--- app/assets/javascripts/views/tracks/track_show.js
+++ app/assets/javascripts/views/tracks/track_show.js
@@ -3,10 +3,23 @@ Soundclone.Views.TrackShow = Backbone.View.extend({
     this.listenTo(this.model, "sync", this.render)
   },
 
+  events: {
+    "click #delete-track": "destroyTrack"
+  },
+
   template: JST['tracks/show'],
 
   render: function () {
     this.$el.html(this.template({track: this.model}));
     return this;
+  },
+
+  destroyTrack: function (event) {
+    event.preventDefault();
+    this.model.destroy({
+      success: function () {
+        Backbone.history.navigate("", {trigger: true});
+      }
+    })
   }
 })

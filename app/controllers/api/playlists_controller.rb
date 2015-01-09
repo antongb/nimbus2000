97c0a32c852e0b3f7a1e8169ede9917345b0db13
Anton Shain <shainanton@gmail.com>
2015-01-09 18:55:43 -0500

    Fixes queries, maybe

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -1,7 +1,12 @@
 class Api::PlaylistsController < ApplicationController
 
   def index
-    @user = User.includes(:playlists).find(params[:user_id])
+    @user = User.find(params[:user_id])
+    if current_user.id == params[:user_id]
+      @playlists = current_user.playlists
+    else
+      @playlists = Playlist.where("private = false AND owner_id = ?", params[:user_id])
+    end
   end
 
 end
